042f12edf022c53e6babb424006b5c4aed6f4133
catherinekimani <catherinekimani882@gmail.com>
2023-12-08 18:18:29 +0300

    Add customer, contact & item models

--- users/models.py
+++ users/models.py
@@ -1,41 +1,111 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
-# Create your models here.
-class Profile(models.Model):
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.timezone import now
+
+class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    user_bio = models.TextField(max_length=150)
-    user_contact = models.EmailField(max_length=100)
-    user_profile = CloudinaryField('image')
-    location = models.CharField(max_length=100,blank=True,null=True)
-    
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=10, null=True, blank=True)
+
     def __str__(self):
-        return f'{self.user} Profile'
+        return str(self.user)
 
 class AuctionItem(models.Model):
-    title = models.CharField(max_length=255)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    sale_price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
-    image = CloudinaryField('items_img')
-    sale = models.BooleanField(default=False)
+    name = models.CharField(max_length=100)
+    price = models.FloatField()
+    image = CloudinaryField('image')
 
     def __str__(self):
-        return self.title
-class ShoppingCartItem(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    item = models.ForeignKey(AuctionItem, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField(default=1)
+        return self.name
+
+class AuctionItemFeature(models.Model):
+    product = models.ForeignKey(AuctionItem, on_delete=models.CASCADE)
+    feature = models.CharField(max_length=1000, null=True, blank=True)
 
     def __str__(self):
-        return f"{self.user.username} - {self.item.title}"
+        return str(self.product) + " Feature: " + self.feature
 
-class Item(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField()
+class AuctionItemReview(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE) 
+    product = models.ForeignKey(AuctionItem, on_delete=models.CASCADE)
+    content = models.TextField()
+    datetime = models.DateTimeField(default=now)
 
-class Bid(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    item = models.ForeignKey(Item, on_delete=models.CASCADE)
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
-    timestamp = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return str(self.customer) +  " Review: " + self.content
+
+class Purchase(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+    date_ordered = models.DateTimeField(default=now)
+    complete = models.BooleanField(default=False)
+
+    def __str__(self):
+        return str(self.id)
+
+    @property
+    def get_cart_total(self):
+        orderitems = self.purchaseitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+
+    @property
+    def get_cart_items(self):
+        orderitems = self.purchaseitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
+
+class PurchaseItem(models.Model):
+    product = models.ForeignKey(AuctionItem, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Purchase, on_delete=models.SET_NULL, null=True)
+    quantity = models.IntegerField(default=0)
+    date_added = models.DateTimeField(default=now)
+
+    def __str__(self):
+        return str(self.order)
+
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
+
+class OrderUpdate(models.Model):
+    order_id = models.ForeignKey(Purchase, on_delete=models.CASCADE)
+    desc = models.CharField(max_length=500)
+    date = models.DateField(default=now)
+
+    def __str__(self):
+        return str(self.order_id)
+
+class PurchaseDetail(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Purchase, on_delete=models.SET_NULL, null=True)
+    phone_number = models.CharField(max_length=10, blank=True, null=True)
+    total_amount = models.CharField(max_length=10, blank=True,null=True)
+    address = models.CharField(max_length=300)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    zipcode = models.CharField(max_length=100)
+    payment = models.CharField(max_length=100, blank=True)
+    date_added = models.DateTimeField(default=now)
+
+    def __str__(self):
+        return self.address
+
+class UserContact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=50)
+    phone = models.CharField(max_length=10)
+    desc = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.name
+
+# class Bid(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     item = models.ForeignKey(Item, on_delete=models.CASCADE)
+#     amount = models.DecimalField(max_digits=10, decimal_places=2)
+#     timestamp = models.DateTimeField(auto_now_add=True)
