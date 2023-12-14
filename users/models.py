775783c6943d16826fbda5872058efd90de9873d
catherinekimani <catherinekimani882@gmail.com>
2023-12-14 10:30:50 +0300

    modify templates

--- users/models.py
+++ users/models.py
@@ -103,9 +103,3 @@ class UserContact(models.Model):
 
     def __str__(self):
         return self.name
-
-# class Bid(models.Model):
-#     user = models.ForeignKey(User, on_delete=models.CASCADE)
-#     item = models.ForeignKey(Item, on_delete=models.CASCADE)
-#     amount = models.DecimalField(max_digits=10, decimal_places=2)
-#     timestamp = models.DateTimeField(auto_now_add=True)
