f5f3d161f8264e44c1f0fe4e6b164305f72a79fa
catherinekimani <catherinekimani882@gmail.com>
2023-12-15 19:19:10 +0300

    create bid form

--- users/forms.py
+++ users/forms.py
@@ -1,26 +1,3 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
-from .models import Profile
-
-class UserRegistrationForm(UserCreationForm):
-
-    email = forms.EmailField()
-
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'password1', 'password2']
-
-class ProfileForm(forms.ModelForm):
-    class Meta:
-        model = Profile
-        fields = ['user_bio','user_profile']
-        
-class ProfileUpdateForm(forms.ModelForm):
-    user_bio = forms.CharField(required=True,widget=forms.TextInput(attrs={'class':'form-control'}))
-    class Meta:
-        model = Profile
-        exclude = ('location', )
-
-class AddToCartForm(forms.Form):
-    quantity = forms.IntegerField(min_value=1, initial=1)
+class BidForm(forms.Form):
+    amount = forms.FloatField(label='Bid Amount')
