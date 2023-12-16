f7440bef8237d2feb1a790e98e24d7d819fa8e9d
catherinekimani <catherinekimani882@gmail.com>
2023-12-16 22:27:51 +0300

    create form to a allow a user to sell products

--- users/forms.py
+++ users/forms.py
@@ -1,3 +1,10 @@
 from django import forms
+from .models import AuctionItem
+# bid form
 class BidForm(forms.Form):
     amount = forms.FloatField(label='Bid Amount')
+# products form
+class AuctionItemForm(forms.ModelForm):
+    class Meta:
+        model = AuctionItem
+        fields = ['name', 'price', 'image']
